129aaf3ca4cd9d2ed0a31ab78508f3e54ac53a04
David Lau <avidlaud@gmail.com>
2021-04-18 14:28:43 -0400

    Remove console logs, add basic heatmap

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -3,26 +3,23 @@ import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
 import './Sidebar.css';
 
-const Sidebar = () => {
-    console.log('Sidebar');
-    return (
-        <div id="sidebar">
-            <Nav className="col-md-12 d-none d-md-block bg-light sidebar">
-                <Nav.Item>
-                    <Nav.Link as={Link} to="/register">Register User</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link as={Link} to="/userlist">User List</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link as={Link} to="/braceletlist">Bracelet List</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link as={Link} to="/locationmap">Map</Nav.Link>
-                </Nav.Item>
-            </Nav>
-        </div>
-    );
-};
+const Sidebar = () => (
+    <div id="sidebar">
+        <Nav className="col-md-12 d-none d-md-block bg-light sidebar">
+            <Nav.Item>
+                <Nav.Link as={Link} to="/register">Register User</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link as={Link} to="/userlist">User List</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link as={Link} to="/braceletlist">Bracelet List</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link as={Link} to="/locationmap">Map</Nav.Link>
+            </Nav.Item>
+        </Nav>
+    </div>
+);
 
 export default Sidebar;
