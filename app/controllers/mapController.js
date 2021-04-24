08e0c330a3856cce0ba118728c13cf3410149ade
David Lau <avidlaud@gmail.com>
2021-04-24 00:34:04 -0400

    Implement heatmaps, demo data

--- app/controllers/mapController.js
+++ app/controllers/mapController.js
@@ -1,4 +1,4 @@
-const { Path, Interaction } = require('../models/mapModel');
+const { Path, Interaction, Heatmap } = require('../models/mapModel');
 
 exports.getPaths = async (req, res) => {
     await Path.find({}, (err, pathList) => {
@@ -11,5 +11,12 @@ exports.getInteractions = async (req, res) => {
     await Interaction.find({}, (err, interactionList) => {
         if (err) throw err;
         else res.send(interactionList);
+    });
+};
+
+exports.getHeatmap = async (req, res) => {
+    await Heatmap.find({}, (err, heatmapList) => {
+        if (err) throw err;
+        else res.send(heatmapList);
     });
 };
--- client/src/components/LocationMap/LocationMap.jsx
+++ client/src/components/LocationMap/LocationMap.jsx
@@ -9,9 +9,11 @@ import {
     PolygonSeries,
     HexbinSeries,
     LineSeries,
+    CustomSVGSeries,
 } from 'react-vis';
 // import React, { useEffect, useState } from 'react';
 import React, { useEffect, useState } from 'react';
+import RangeSlider from 'react-bootstrap-range-slider';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import './LocationMap.css';
@@ -23,8 +25,14 @@ const LocationMap = () => {
     const [showHeatmap, setShowHeatmap] = useState(false);
     const [showPaths, setShowPaths] = useState(false);
     const [showGrid, setShowGrid] = useState(false);
+    const [showBeacons, setShowBeacons] = useState(false);
+    const [showContacts, setShowContacts] = useState(false);
+    const [showDemo, setShowDemo] = useState(false);
+
+    const [demoValue, setDemoValue] = useState(0);
 
     const [paths, setPaths] = useState([]);
+    const [heatmapData, setHeatmapData] = useState([]);
 
     const toggleObjects = () => {
         setShowObjects(!showObjects);
@@ -46,12 +54,214 @@ const LocationMap = () => {
         setShowPaths(!showPaths);
     };
 
+    const toggleBeacons = () => {
+        setShowBeacons(!showBeacons);
+    };
+
+    const toggleContacts = () => {
+        setShowContacts(!showContacts);
+    };
+
+    const toggleShowDemo = () => {
+        setShowDemo(!showDemo);
+    };
+
     const getPathData = async () => {
         const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/map/paths`, {
             method: 'GET',
         });
         setPaths(await response.json());
     };
+
+    const getHeatmapData = async () => {
+        const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/map/heatmap`, {
+            method: 'GET',
+        });
+        setHeatmapData(await response.json());
+    };
+
+    const contactData = [
+        { x: 10, y: 35 },
+        { x: 10, y: 38 },
+        { x: 10, y: 45 },
+        { x: 17, y: 47 },
+        { x: 20, y: 50 },
+        { x: 27, y: 32 },
+        { x: 60, y: 16 },
+        { x: 65, y: 30 },
+        { x: 47, y: 45 },
+    ];
+
+    const demoLocations = [
+        [
+            { x: 10, y: 1, color: 'green' },
+        ],
+        [
+            { x: 10, y: 5, color: 'green' },
+        ],
+        [
+            { x: 10, y: 9, color: 'green' },
+        ],
+        [
+            { x: 10, y: 13, color: 'green' },
+        ],
+        [
+            { x: 10, y: 17, color: 'green' },
+        ],
+        [
+            { x: 10, y: 21, color: 'green' },
+        ],
+        [
+            { x: 10, y: 25, color: 'green' },
+            { x: 10, y: 1, color: 'orange' },
+        ],
+        [
+            { x: 10, y: 29, color: 'green' },
+            { x: 10, y: 6, color: 'orange' },
+        ],
+        [
+            { x: 10, y: 34, color: 'green' },
+            { x: 10, y: 12, color: 'orange' },
+        ],
+        [
+            { x: 17, y: 34, color: 'green' },
+            { x: 10, y: 18, color: 'orange' },
+        ],
+        [
+            { x: 18, y: 34, color: 'green' },
+            { x: 10, y: 18, color: 'orange' },
+        ],
+        [
+            { x: 18, y: 34, color: 'green' },
+            { x: 10, y: 24, color: 'orange' },
+        ],
+        [
+            { x: 18, y: 34, color: 'green' },
+            { x: 10, y: 30, color: 'orange' },
+        ],
+        [
+            { x: 18, y: 38, color: 'green' },
+            { x: 10, y: 36, color: 'orange' },
+        ],
+        [
+            { x: 18, y: 38, color: 'green' },
+            { x: 10, y: 41, color: 'orange' },
+        ],
+        [
+            { x: 18, y: 38, color: 'green' },
+            { x: 10, y: 51, color: 'orange' },
+        ],
+        [
+            { x: 18, y: 41, color: 'green' },
+            { x: 15, y: 48, color: 'orange' },
+        ],
+        [
+            { x: 18, y: 44, color: 'green' },
+            { x: 15, y: 47, color: 'orange' },
+        ],
+        [
+            { x: 18, y: 45, color: 'red' },
+            { x: 16, y: 47, color: 'red' },
+        ],
+        [
+            { x: 18, y: 45, color: 'red' },
+            { x: 16, y: 47, color: 'red' },
+        ],
+        [
+            { x: 18, y: 44, color: 'green' },
+            { x: 15, y: 47, color: 'orange' },
+        ],
+        [
+            { x: 19, y: 47, color: 'green' },
+            { x: 13, y: 47, color: 'orange' },
+        ],
+        [
+            { x: 26, y: 47, color: 'green' },
+            { x: 17, y: 45, color: 'orange' },
+        ],
+        [
+            { x: 26, y: 42, color: 'green' },
+            { x: 18, y: 40, color: 'orange' },
+        ],
+        [
+            { x: 26, y: 42, color: 'green' },
+            { x: 18, y: 35, color: 'orange' },
+        ],
+        [
+            { x: 26, y: 42, color: 'green' },
+            { x: 18, y: 27, color: 'orange' },
+        ],
+        [
+            { x: 26, y: 36, color: 'green' },
+            { x: 20, y: 20, color: 'orange' },
+        ],
+        [
+            { x: 26, y: 23, color: 'green' },
+            { x: 21, y: 16, color: 'orange' },
+        ],
+        [
+            { x: 36, y: 23, color: 'green' },
+            { x: 24, y: 16, color: 'orange' },
+        ],
+        [
+            { x: 36, y: 30, color: 'green' },
+            { x: 24, y: 11, color: 'orange' },
+        ],
+        [
+            { x: 36, y: 36, color: 'green' },
+            { x: 24, y: 11, color: 'orange' },
+        ],
+        [
+            { x: 36, y: 47, color: 'green' },
+            { x: 24, y: 11, color: 'orange' },
+        ],
+        [
+            { x: 46, y: 47, color: 'green' },
+            { x: 24, y: 11, color: 'orange' },
+        ],
+        [
+            { x: 46, y: 37, color: 'green' },
+            { x: 24, y: 6, color: 'orange' },
+        ],
+        [
+            { x: 46, y: 32, color: 'green' },
+            { x: 24, y: 3, color: 'orange' },
+        ],
+        [
+            { x: 46, y: 22, color: 'green' },
+            { x: 24, y: 1, color: 'orange' },
+        ],
+        [
+            { x: 50, y: 20, color: 'green' },
+        ],
+        [
+            { x: 48, y: 17, color: 'green' },
+        ],
+        [
+            { x: 48, y: 11, color: 'green' },
+        ],
+        [
+            { x: 48, y: 11, color: 'green' },
+        ],
+        [
+            { x: 48, y: 11, color: 'green' },
+        ],
+        [
+            { x: 48, y: 11, color: 'green' },
+        ],
+        [
+            { x: 48, y: 6, color: 'green' },
+        ],
+        [
+            { x: 48, y: 3, color: 'green' },
+        ],
+        [
+            { x: 48, y: 1, color: 'green' },
+        ],
+        [],
+    ];
+
+    console.log(demoLocations.length);
 
     const locations = [
         // {
@@ -61,15 +271,66 @@ const LocationMap = () => {
         // },
     ];
 
-    const heatmapData = [
-        { x: 10, y: 30 },
-    ];
+    // const popularAisles = []
+
+    // const row = [];
+    // for (let y = 1; y <= 50; y += 1) {
+    //     row.push({ x: 10, y });
+    // }
+
+    // const customHeatmapData = [
+    //     { x: 10, y: 30 },
+    //     { x: 10, y: 30 },
+    //     { x: 10, y: 30 },
+    //     { x: 10, y: 30 },
+    //     { x: 10, y: 30 },
+    //     { x: 10, y: 30 },
+    //     { x: 15, y: 35 },
+    //     { x: 15, y: 35 },
+    //     { x: 15, y: 35 },
+    //     { x: 20, y: 30 },
+    //     { x: 20, y: 30 },
+    //     { x: 20, y: 30 },
+    // ];
+
+    // const heatmapData = customHeatmapData.concat(row);
 
     const storeBorders = [
         { x: 0, y: 0 },
         { x: 0, y: 60 },
         { x: 90, y: 60 },
         { x: 90, y: 0 },
+    ];
+
+    const beacons = [
+        { x: 5, y: 40 },
+        { x: 5, y: 48 },
+        { x: 15, y: 53 },
+        { x: 72, y: 53 },
+        { x: 67, y: 41 },
+        { x: 59, y: 47 },
+        { x: 52, y: 38 },
+        { x: 21, y: 42 },
+        { x: 21, y: 26 },
+        { x: 31, y: 34 },
+        { x: 42, y: 26 },
+        { x: 42, y: 44 },
+        { x: 22, y: 12 },
+        { x: 28, y: 12 },
+        { x: 34, y: 12 },
+        { x: 40, y: 12 },
+        { x: 46, y: 12 },
+        { x: 52, y: 12 },
+        { x: 58, y: 12 },
+        { x: 64, y: 12 },
+        { x: 80, y: 42 },
+        { x: 82, y: 31 },
+        { x: 80, y: 19 },
+        { x: 6, y: 21 },
+        { x: 6, y: 9 },
+        { x: 72, y: 25 },
+        { x: 74, y: 12 },
+        { x: 63, y: 27 },
     ];
 
     const storeObjects = [
@@ -325,7 +586,8 @@ const LocationMap = () => {
 
     useEffect(() => {
         getPathData();
-    });
+        getHeatmapData();
+    }, []);
 
     // useEffect(() => {
     //     const formattedLocations = getLocations().then((locationdata) => {
@@ -448,6 +710,7 @@ const LocationMap = () => {
                         {showHeatmap && (
                             <HexbinSeries
                                 animation
+                                sizeHexagonsWithCount
                                 data={heatmapData}
                                 style={{
                                     opacity: 0.8,
@@ -467,6 +730,40 @@ const LocationMap = () => {
                                     }}
                                 />
                             ))
+                        )}
+                        {false && (
+                            beacons.map((beacon) => (
+                                <CustomSVGSeries
+                                    data={beacon}
+                                    customComponent="square"
+                                />
+                            ))
+                        )}
+                        {showBeacons && (
+                            <CustomSVGSeries
+                                animation
+                                data={beacons}
+                                customComponent="square"
+                                size={10}
+                            />
+                        )}
+                        {showContacts && (
+                            <CustomSVGSeries
+                                animation
+                                data={contactData}
+                                customComponent="diamond"
+                                size={20}
+                                style={{
+                                    fill: 'red',
+                                }}
+                            />
+                        )}
+                        {showDemo && (
+                            <MarkSeries
+                                animation
+                                colorType="literal"
+                                data={demoLocations[demoValue]}
+                            />
                         )}
                     </XYPlot>
                     <Button
@@ -499,6 +796,29 @@ const LocationMap = () => {
                     >
                         Toggle Paths
                     </Button>
+                    <Button
+                        bsPrefix="custom-button"
+                        onClick={toggleBeacons}
+                    >
+                        Toggle Beacons
+                    </Button>
+                    <Button
+                        bsPrefix="custom-button"
+                        onClick={toggleContacts}
+                    >
+                        Show Contact Locations
+                    </Button>
+                    <Button
+                        bsPrefix="custom-button"
+                        onClick={toggleShowDemo}
+                    >
+                        Show Demo
+                    </Button>
+                    <RangeSlider
+                        value={demoValue}
+                        max={demoLocations.length - 1}
+                        onChange={(changeEvent) => setDemoValue(changeEvent.target.value)}
+                    />
                 </Container>
             </div>
         </div>
